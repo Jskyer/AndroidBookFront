506fcd0ae335d6afe443bcfa9d07d7c41814f19b
Sophie-Sumner <134195205+Sophie-Sumner@users.noreply.github.com>
2023-11-09 21:47:40 +0800

    Play bar
    Need to be modified

--- app/src/main/java/com/example/newhelloworld/MainActivity.java
+++ app/src/main/java/com/example/newhelloworld/MainActivity.java
@@ -1,17 +1,73 @@
 package com.example.newhelloworld;
 
-import android.os.Bundle;
+import static android.opengl.ETC1.getHeight;
+import static android.opengl.ETC1.getWidth;
 
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.newhelloworld.views.AudioRoundProgressView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.textview.MaterialTextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * 底部logo图标，点击之后弹出当前播放歌曲详情页
+     */
+    private ShapeableImageView ivLogo;
+    /**
+     * 底部当前播放歌名
+     */
+    private MaterialTextView tvSongName;
+    /**
+     * 底部当前歌曲控制按钮, 播放和暂停
+     */
+    private MaterialButton btnPlay;
+    /**
+     * 音频播放器
+     */
+    private MediaPlayer mediaPlayer;
+    /**
+     * 记录当前播放歌曲的位置
+     */
+    public int mCurrentPosition = -1;
+
+    /**
+     * 自定义进度条
+     */
+    private AudioRoundProgressView musicProgress;
+
+    /**
+     * 音乐进度间隔时间
+     */
+    private static final int INTERNAL_TIME = 1000;
+
+    /**
+     * 图片动画
+     */
+    private ObjectAnimator logoAnimation;
+
+
+
     BottomNavigationView bottomNavigation;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +76,12 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigation = findViewById(R.id.bottom_navigation);
 
+
         //获取navController
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         //通过setupWithNavController将底部导航和导航控制器进行绑定
         NavigationUI.setupWithNavController(bottomNavigation,navController);
+
+
     }
 }
